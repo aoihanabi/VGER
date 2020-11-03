ce2010bd213fb8c825ec9efcd321525f671c3a7e
aoihanabi <abii98cm@gmail.com>
2020-11-03 14:10:37 -0600

    Created a model and controller for Category Module &  added methods for the DB relationship w/ products

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The products that belong to the category.
+     */
+    public function products()
+    {
+        return $this->belongsToMany(Product::Class, 'prodxcateg')->withTimestamps();
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Models\Image;
 use App\Models\Attribute;
+use App\Models\Category;
+use App\Models\Image;
 
 class Product extends Model
 {
@@ -18,16 +19,30 @@ class Product extends Model
    */
   public function images()
   {
-      return $this->hasMany(Image::Class);
+    return $this->hasMany(Image::Class);
   }
 
+  /**
+   * Scope to retrieve a product's main image.
+   */
   public function main_image() 
   {
     return $this->hasMany(Image::Class)->where('type', 'MN');
   }
 
+  /**
+   * The categories that belong to the product
+   */
+  public function categories()
+  {
+    return $this->belongsToMany(Category::Class, 'prodxcateg')->withTimestamps();
+  }
+  
+  /**
+   * The values (or properties) that belong to the product
+   */
   public function values() 
   {
-    return $this->belongsToMany(Attribute::Class, 'values')->withPivot('option_id');
+    return $this->belongsToMany(Attribute::Class, 'values')->withPivot('option_id')->withTimestamps();
   }
 }
