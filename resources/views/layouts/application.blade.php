3c56ca70ba2e2606eacf91b5049dc0493b16cd50
aoihanabi <abii98cm@gmail.com>
2020-11-05 17:39:15 -0600

    Started using Tailwind for styling in application layout and product's index page

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <!--Styles-->
+    
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     
     <!--Scripts-->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -13,22 +15,23 @@
   </head>
   <body>
     <div id="app">
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="{{ route('products.index') }}">Products</a></li>
-          <li><a href="{{ route('login') }}">Login</a></li>
-        </ul>
-      </nav>
+      <div class="relative bg-white flex flex-row border-solid border-b border-gray-300">
+        <div class="p-5 justify-start"><p class="text-xl">Logo</p></div>
+        <div class="p-5 justify-end flex flex-row flex-auto ">
+          
+          <div class="mx-2 text-gray-800"><a href="/">Home</a></div>
+          <div class="mx-2 text-gray-800"><a href="{{ route('products.index') }}">Products</a></div>
+          <div class="mx-2 text-gray-800"><a href="{{ route('login') }}">Login</a></div>
+        </div>
+      </div>
 
+      <div  class="">
+        @yield('content')        
+      </div>
       <div id="navbarExampleTransparentExample" class="navbar-menu"> 
         <div class="navbar-end">
           <order-component />
         </div>
-      </div>
-
-      <div>
-        @yield('content')        
       </div>
     </div>
   </body>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,35 +1,39 @@
 @extends('layouts.application')
 
 @section('content')
-  
-  <h1>Productos Disponibles</h1>
-  <hr>
-  <meta name="csrf-token" content="{{ csrf_token() }}" />
-  
-  @can('create', App\Models\Product::class)
-    <a href="products/create">Crear nuevo producto</a>
-    {{ link_to(route('categories.create'), $title = 'Crear nueva categoría') }}
-    {{ link_to(route('options.create'), $title = 'Crear nueva característica') }}
-  @endcan
-
-  @foreach($products as $key => $prod)
-    <div>
-      <img src="{{ asset($main_imgs[$key]) }}" style="width: 300px; height: 200px;"></img>
-      <br>
-      {{ link_to("products/$prod->id", $prod->name, $attributes = [], $secure = null) }}
-      @can('create', App\Models\Product::class)
-        <div class="prod_quantity_update">
-          {{ Form::label('quantity', 'Disponible') }}
-          {{ Form::number('quantity', "$prod->quantity", ['class' => 'quantity-modifier', 'id' => "$prod->quantity", 'min' => '0']) }}
-          {{ Form::button('Save', ['class' => 'save-quantity-changes', 'hidden']) }}
-          {{ Form::hidden('prod_id', null, ['id' => "$prod->id"])}}
-        </div>
-      @endcan
-      <product-list :product="{{ $prod }}"/>
+  <div class="my-10 mx-20">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <div id="product_masthead" class="py-5 text-center">
+      <h1 class="m-5 text-xl">Productos Disponibles</h1>
     </div>
-    <hr>
-  @endforeach
-  <br>
-  <hr>
+    
+    @can('create', App\Models\Product::class)
+      <a href="products/create">Crear nuevo producto</a>
+      {{ link_to(route('categories.create'), $title = 'Crear nueva categoría') }}
+      {{ link_to(route('options.create'), $title = 'Crear nueva característica') }}
+    @endcan
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      @foreach($products as $key => $prod)
+        <div class="border-solid border border-gray-300 rounded">
+          <div class="m-5">
+            <img src="{{ asset($main_imgs[$key]) }}" style="width: 300px; height: 200px;"></img>
+            <br>
+            {{ link_to("products/$prod->id", $prod->name, $attributes = [], $secure = null) }}
+            @can('create', App\Models\Product::class)
+              <div class="prod_quantity_update">
+                {{ Form::label('quantity', 'Disponible') }}
+                {{ Form::number('quantity', "$prod->quantity", ['class' => 'quantity-modifier', 'id' => "$prod->quantity", 'min' => '0']) }}
+                {{ Form::button('Save', ['class' => 'save-quantity-changes', 'hidden']) }}
+                {{ Form::hidden('prod_id', null, ['id' => "$prod->id"])}}
+              </div>
+            @endcan
+            <product-list :product="{{ $prod }}"/>
+          </div>
+          
+        </div>
+      @endforeach
+    </div>
+  </div>
+  
   
 @endsection
