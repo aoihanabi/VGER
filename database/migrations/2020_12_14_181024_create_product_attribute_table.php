bee6d00f0f05c255d1a7520b8fba77f19ccce205
aoihanabi <abii98cm@gmail.com>
2020-12-15 10:02:45 -0600

    Added migrations for new tables that will store product's attributes and options(with corresponding amount)

---
+++ database/migrations/2020_12_14_181024_create_product_attribute_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductAttributeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            Schema::dropIfExists('values');
+            $table->id();
+            $table->foreignId('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreignId('attribute_id');
+            $table->foreign('attribute_id')->references('id')->on('attributes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attributes');
+    }
+}
