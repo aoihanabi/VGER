5a78727fcc5464a80e659010f49494a528ed34e0
aoihanabi <abii98cm@gmail.com>
2020-12-24 10:36:25 -0600

    Fixed repeated options on product show page dropdowns

--- resources/views/product/_options_dropdown.blade.php
+++ resources/views/product/_options_dropdown.blade.php
@@ -1,12 +1,21 @@
 <select id="{{ strtolower($attr->name) }}_selected" name="drop_option" class="buy_dropdown p-1 col-span-3 border-2 border-gray-400 rounded">
     <!-- <option></option> -->
+    @php
+        $duplicates = [];        
+    @endphp
     @foreach ($options_db as $detail) 
         <!-- <p>{{ $detail->options_ids }}</p> -->
         
         @foreach((array)json_decode($detail->options_ids) as $key => $opt)
             @if($opt != null && $key == strtolower($attr->name))
-
-                <option value="{{ $opt->id }}" >{{ $opt->option }}</option>
+                
+                @if (!in_array($opt->id, $duplicates))
+                    <option value="{{ $opt->id }}" >{{ $opt->option }}</option>
+                    @php 
+                        $duplicates[] = $opt->id;
+                    @endphp
+                @endif
+                
 
             @endif
         @endforeach
