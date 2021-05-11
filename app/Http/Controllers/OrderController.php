405de83e9b9217422c73691d9e6d2b56329d449a
aoihanabi <abii98cm@gmail.com>
2021-05-11 10:00:12 -0600

    Included functionality to update the product options amount after an order is proccessed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\ProductOptions;
 use App\Mail\OrderProcessed;
 
 class OrderController extends Controller
@@ -51,44 +52,57 @@ class OrderController extends Controller
             //print_r($full_order);
             
             if(!empty($full_order)) {
+                
                 $total = 0;
                 $order = new Order;
                 $order->date = now();
                 $order->user_id = Auth::user()->id;
                 $order->total = 0;
-                $order->save();
+                // $order->save();
 
                 $ord_id = Order::latest()->first();
                 
                 foreach($full_order as $prod) {
-
+                    
                     $product = Product::find($prod->id, ['id']);
-                    $purchased_quantity = $prod->totalProdAmount;
-                    $subtotal = $prod->totalProdPrice;
+                    
                     foreach($prod->details as $detail) {
+                        
                         $description = "";
-                        foreach($detail->description as $descrip_item) {
-                            $description .= $descrip_item->label . " ";
+                        $wheres = array();
+                        foreach($detail->description as $key => $option_item) {
+                            // Create the product description for the order
+                            $description .= $option_item->label . " ";
+                            
+                            // Build the 'where clause' with the color, talla, estilo ids, to find the specific product option
+                            $wheres[] = ["options_ids->{$key}->id", $option_item->id];
                         }
+                        $productOption = ProductOptions::where('product_id', $product->id)
+                                        ->where($wheres)->first(); 
+                        $productOption->amount = $detail->option_amount_left; //Update specific product option amount left
+                        $productOption->save();
+
+                        $purchased_quantity = $detail->cart_amount;
+                        $subtotal = $detail->total_price;
+                        $product->orders()->attach($ord_id->id, ['subtotal' => $subtotal, 'purchased_quantity' => $purchased_quantity, 'description' => $description]);
+                        
+                        $total += $subtotal;
                     }
-                    $total += $subtotal;
-                    $product->orders()->attach($ord_id->id, ['subtotal' => $subtotal, 'purchased_quantity' => $purchased_quantity, 'description' => $description]);
-                    # echo("Left: ".$prod->quantityLeft . PHP_EOL);
-                    $product->quantity = $prod->quantityLeft;
+                    
+                    $product->quantity = $prod->quantityLeft; // Update the general product amount left
                     $product->save();
                 }
                 $order->total = $total;
-                #print_r($order);
                 $order->save();
 
-                // send_email('Su pedido se realizó con éxito!',
-                //             'El total de su compra es ' . $total . ' haha. Gracias',
-                //             Auth::user()->email);
+                // // send_email('Su pedido se realizó con éxito!',
+                // //             'El total de su compra es ' . $total . ' haha. Gracias',
+                // //             Auth::user()->email);
                 $details = Order::get_order_details($order->id);
                 Mail::to(Auth::user()->email)->send(new OrderProcessed($order, $details));
                 $request->session()->flash('order_notification', "¡Pedido procesado con éxito!");
             }
-            
+            //return response($t);
             //return redirect()->action([OrderController::class, 'index'], 200)->with('order_notification', 'Pedido procesado con éxito');
             
         } else {
